8eef328aa86a026518c05187bb8e7ab90693dc48
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-10-14 12:00:18 -0700

    Implement functionalities required by LighthouseBnB Users section

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -17,16 +17,24 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  const queryString = `
+  SELECT *
+  FROM users
+  WHERE LOWER(users.email) = LOWER($1);
+  `;
+
+  const values = [email];
+
+  return pool
+    .query(queryString, values)
+    .then(res => {
+      if (res.rows.length != 0) {
+        return res.rows[0];
+      }
+
+      return null;
+    })
+    .catch(err => console.log(err.message));
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -36,7 +44,24 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  const queryString = `
+  SELECT *
+  FROM users
+  WHERE users.id = $1;
+  `;
+
+  const values = [id];
+
+  return pool
+    .query(queryString, values)
+    .then(res => {
+      if (res.rows.length != 0) {
+        return res.rows[0];
+      }
+
+      return null;
+    })
+    .catch(err => console.log(err.message));
 }
 exports.getUserWithId = getUserWithId;
 
@@ -47,10 +72,18 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const queryString = `
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `;
+
+  const values = [user.name, user.email, user.password];
+
+  return pool
+    .query(queryString, values)
+    .then(res => res.rows[0])
+    .catch(err => console.log(err.message));
 }
 exports.addUser = addUser;
 
